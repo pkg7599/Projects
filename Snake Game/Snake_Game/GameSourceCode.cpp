9d58398b006e20381d0912b1749eb740dd47ea53
Pranjal Kumar Gupta <38903208+pkg7599@users.noreply.github.com>
2020-12-21 21:47:55 +0530

    Project_3
    Snake Game in CPP language.

---
+++ Snake Game/Snake_Game/GameSourceCode.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+#include<stdlib.h>      //for rand()
+#include <conio.h>      //for system(),_kbhit(),_getch()
+#include <windows.h>    //for Sleep()
+
+using namespace std;
+
+bool gameOver = false;
+const int width = 24;
+const int height = 24;
+int x, y, fruitX, fruitY, score;
+int tailX[500], tailY[500];
+int nTail;
+enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirecton dir;
+
+void Setup()
+{
+    if(!gameOver)
+        gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+    nTail = 0;
+}
+
+void Draw()
+{
+    system("cls");
+    cout << "\tSnake Game" << endl;
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (gameOver) {
+                if (j == (width) / 2 - 4 && i == (height + 2) / 2) {
+                    cout << "GAME OVER";
+                }
+            }
+            if (j == 0)
+                cout << "#";
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "$";
+            else
+            {
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        cout << "O";
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " ";
+            }
+
+
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+    cout << "Score:" << score << endl;
+}
+
+void Input()
+{
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'z':
+            gameOver = true;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void Logic()
+{
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+    //if (x > width || x < 0 || y > height || y < 0)
+    //  gameOver = true;
+    if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+
+int main()
+{
+    Setup();            //Intializing values of game parameter.
+    while (!gameOver)
+    {
+        Draw();         //Creating the game environment.
+        Input();        //Checking for Input.
+        Logic();        //Running game logic.
+     //   Sleep(10);
+    }
+    if (gameOver) {
+        Setup();
+        Draw();
+    }
+  // _getch();
+    return 0;
+}
